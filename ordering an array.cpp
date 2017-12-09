25dd92422c03be791ac76dcd9fe6150de37cf67b
Decio-Greenwood <decioedward@hotmail.com>
2017-12-09 22:04:16 +0100

    basic order code

--- ordering an array.cpp
+++ ordering an array.cpp
@@ -3,20 +3,21 @@
 using namespace std;
 
 
+
 int main () {
 	int array[5], support;
-	
+
 	for (int x=0; x<5; x++) {
 		cout << "Insert the element "<< x+1 << " of your array:\n";
 		cin >> array[x];
 	}
-	
+
 	cout<< "\nYour original array is:";
 	for (int x=0; x<5; x++) {
 		cout << " " << array[x] << ",";
 	}
-	
-	
+
+
 	for (int x=0; x<5; x++) {
 		for (int x=0; x<4; x++){
 			if (array[x]>array[x+1]) {
@@ -26,13 +27,13 @@ int main () {
 			}
 		}
 	}
-	
+
 	cout<< "\n\nYour ordered array is:";
 	for (int x=0; x<5; x++) {
 		cout << " " << array[x] << ",";
 	}
-		
-	
+
+
 	return 0;
-	
+
 }
